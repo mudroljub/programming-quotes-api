2066304506f2cd97a708af8c567fc9cb169b3737
Damjan Pavlica <mudroljub@gmail.com>
2017-11-02 19:45:47 +0100

    m

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Baza filmova je u izgradnji'))
+app.get('/', (req, res) => res.send(`Baza filmova je u izgradnji. Trenutno okruzenje je ${process.env.OKRUZENJE}.`))
 
 app.listen(port, () => console.log(`Server sluzi na kapiji ${port}.`))
