1175a26bb2a159598b7c0674ff6cc7c7f543c2ac
Damjan Pavlica <mudroljub@gmail.com>
2017-12-02 00:13:32 +0100

    fix websocket

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const http = require('http')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const WebSocket = require('ws')
 
 const filmovi = require('./rute/filmovi')
 const dodajFilm = require('./rute/dodaj-film')
@@ -11,7 +10,6 @@ const obrisiFilm = require('./rute/obrisi-film')
 const port = process.env.PORT || 5000
 const app = express()
 const server = http.createServer(app)
-const wss = new WebSocket.Server({server})
 
 /* CONFIG */
 
@@ -24,10 +22,8 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Baza podataka u izgradnji.'))
 
 app.get('/filmovi/', filmovi)
-
-app.post('/dodaj-film/', (req, res) => dodajFilm(req, res, wss))
-
 app.get('/obrisi-film/:id', obrisiFilm)
+app.post('/dodaj-film/', (req, res) => dodajFilm(req, res, server))
 
 /* SERVER */
 
--- rute/dodaj-film.js
+++ rute/dodaj-film.js
@@ -1,9 +1,11 @@
 const mongodb = require('mongodb')
 const mongoUri = 'mongodb://filmovi:filmovi@ds243285.mlab.com:43285/heroku_sljlvq37'
+const WebSocket = require('ws')
 
-const dodajFilm = (req, res, wss) => {
+const dodajFilm = (req, res, server) => {
   const {naziv, godina, slika} = req.body
   if (!naziv || !godina || !slika) return res.send('Niste poslali sva polja.')
+
   mongodb.MongoClient.connect(mongoUri, (err, db) => {
     if(err) throw err
     db.collection('filmovi').update(
@@ -12,7 +14,7 @@ const dodajFilm = (req, res, wss) => {
        {upsert: true}
     )
     res.send('Hvala na azuriranju baze filmova.')
-    wss.clients.forEach(client => {
+    new WebSocket.Server({server}).clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) client.send('Baze filmova je azurirana.')
     })
   })
