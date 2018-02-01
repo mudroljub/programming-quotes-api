8624c42e5852eaf93d57b24b1e981b4c50358302
mudroljub <mudroljub@gmail.com>
2018-02-01 23:31:57 +0100

    ocena radi

--- rute/oceni-citat.js
+++ rute/oceni-citat.js
@@ -3,23 +3,27 @@ const ObjectId = require('mongodb').ObjectId
 const mongoUri = require('../config/constants.js').mongoUri
 
 const oceniCitat = (req, res) => {
-  const {_id, novaOcena} = req.body
-  const uslov = (_id && novaOcena)
-  if (!uslov) return res.send('Niste poslali obavezna polja.')
+  const _id = req.body._id
+  const novaOcena = Number(req.body.novaOcena)
+  if (!_id || !novaOcena) return res.send('Niste poslali obavezna polja.')
 
   mongodb.MongoClient.connect(mongoUri, (err, db) => {
     if(err) throw err
+
     db.collection('citati').findOne({_id: new ObjectId(_id)}, (err, citat) => {
       if (err) throw err
       const {glasalo, ocena} = citat
-      res.send(`glasalo: ${glasalo}, ocena: ${ocena}, novaOcena: ${novaOcena}`)
       const noviProsek = (glasalo * ocena + novaOcena) / (glasalo + 1)
-      // res.send(`glasalo: ${glasalo}, ocena: ${ocena}, novaOcena: ${novaOcena}`)
       db.collection('citati').update(
         {_id: new ObjectId(_id)},
-        {$set: {ocena: noviProsek, glasalo: glasalo + 1}}
+        {
+          $set: {
+            ocena: noviProsek.toFixed(1), 
+            glasalo: glasalo + 1
+          }
+        }
       )
-      // res.send('noviProsek: ' + noviProsek)
+      res.send(`Novi prosek je: ${noviProsek.toFixed(1)}`)
       db.close()
     })
   })
