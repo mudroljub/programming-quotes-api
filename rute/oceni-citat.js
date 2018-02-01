584f2e86b105803322388d5f02cdc9323529587b
mudroljub <mudroljub@gmail.com>
2018-02-01 01:49:14 +0100

    ne radi ocena

--- rute/oceni-citat.js
+++ rute/oceni-citat.js
@@ -1,21 +1,32 @@
 const mongodb = require('mongodb')
+const ObjectId = require('mongodb').ObjectId
 const mongoUri = require('../config/constants.js').mongoUri
 
 const oceniCitat = (req, res) => {
   const {_id, novaOcena} = req.body
   const uslov = (_id && novaOcena)
   if (!uslov) return res.send('Niste poslali obavezna polja.')
-  const ocena = novaOcena // implementirati logiku
-  // naci citati
-  // videti broj glasova
 
   mongodb.MongoClient.connect(mongoUri, (err, db) => {
     if(err) throw err
     db.collection('citati').update(
       {_id},
-      {$set: {ocena}}
+      {$set: {ocena: 4.5, glasalo: 2}}
     )
-    res.send('Hvala na azuriranju baze citata.')
+    res.send('Hvala na oceni.')
+
+    // db.collection('citati').findOne({_id: ObjectId(_id)}, (err, citat) => {
+    //   if (err) throw err
+    //   const {glasalo, ocena} = citat
+    //   const noviProsek = (glasalo * ocena + novaOcena) / (glasalo + 1)
+    //   db.collection('citati').update(
+    //     {_id},
+    //     {$set: {ocena: noviProsek, glasalo: glasalo + 1}}
+    //   )
+    //   res.send(''+noviProsek)
+    //   db.close()
+    // })
+
   })
 }
 
