904c1bb4de7e8ebd079eb409c6384e70ab30b4ef
Damjan Pavlica <mudroljub@gmail.com>
2018-03-15 23:56:51 +0100

    wip

--- routes/auth.js
+++ routes/auth.js
@@ -6,14 +6,14 @@ const {clientDomain} = require('../config/host')
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-  const token = jwt.sign({ user: req.user}, process.env.JWTSECRET)
+  const token = jwt.sign({user: req.user}, process.env.JWTSECRET)
   res.redirect(`${clientDomain}/#/auth/google/${token}`)
 })
 
 router.get('/google/:token', (req, res) => {
-  jwt.verify(req.params.token, process.env.JWTSECRET, (err, token) => {
+  jwt.verify(req.params.token, process.env.JWTSECRET, (err, user) => {
     if (err) res.send({res: false})
-    res.send(token)
+    res.send(user)
   })
 })
 
