1d4856f966202707eed3a0c1b8640708c64bafb9
Damjan Pavlica <mudroljub@gmail.com>
2018-03-20 02:04:39 +0100

    sync voted

--- routes/users/update-votes.js
+++ routes/users/update-votes.js
@@ -1,14 +1,13 @@
 const User = require('../../models/User')
 const jwt = require('jsonwebtoken')
 
-module.exports = (req, res) => {
-  const {localVotes, token} = req.body
+module.exports = (req) => {
+  const {voted, token} = req.body
   jwt.verify(token, process.env.JWTSECRET, (err, data) => {
     if (!data.user) return
     User.findById(data.user._id, (err, user) => {
-      const voted = [...new Set([...user.voted, ...localVotes])]
       user.set({ voted })
-      user.save(() => res.send(voted))
+      user.save()
     })
   })
 }
