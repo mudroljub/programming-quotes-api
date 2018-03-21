643b4082df49b8702f3e1adbf57aafd94b0998a3
Damjan Pavlica <mudroljub@gmail.com>
2018-03-21 12:00:04 +0100

    handle error

--- routes/users/update-votes.js
+++ routes/users/update-votes.js
@@ -1,10 +1,11 @@
 const User = require('../../models/User')
 const jwt = require('jsonwebtoken')
 
-module.exports = (req) => {
+module.exports = (req, res) => {
   const {voted, token} = req.body
+  if (!token) return res.send('No token.')
   jwt.verify(token, process.env.JWTSECRET, (err, data) => {
-    if (!data.user) return
+    if (!data || !data.user) return res.send('No user data.')
     User.findById(data.user._id, (err, user) => {
       user.set({ voted })
       user.save()
