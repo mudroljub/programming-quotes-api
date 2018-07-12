8866e18299aa45204c49cf49f089bb96ea737c96
Damjan Pavlica <mudroljub@gmail.com>
2018-07-13 01:11:12 +0200

    referer

--- config/host.js
+++ config/host.js
@@ -4,12 +4,7 @@ const domain = process.env.NODE_ENV === 'development'
   ? `http://localhost:${port}`
   : 'https://baza-podataka.herokuapp.com'
 
-const clientDomain = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000'
-  : 'https://skolakoda.org/programming-quotes'
-
 module.exports = {
   port,
   domain,
-  clientDomain
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,12 @@
 const router = require('express').Router()
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
-const {clientDomain} = require('../config/host')
 
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
   const token = jwt.sign({user: req.user}, process.env.JWTSECRET)
-  res.redirect(`${req.headers.referer}/#/auth/google/${token}`)
+  res.redirect(`${req.headers.referer}#/auth/google/${token}`)
 })
 
 router.get('/google/:token', (req, res) => {
