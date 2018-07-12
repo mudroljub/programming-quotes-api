16cfb1910f749bc180e2d5be4dfc40137fdf9a06
Damjan Pavlica <mudroljub@gmail.com>
2018-07-12 23:59:33 +0200

    update api

--- routes/thoughts/create.js
+++ routes/thoughts/create.js
@@ -3,14 +3,14 @@ const SacredThought = require('../../models/SacredThought')
 module.exports = (req, res) => {
   const {en, sr, author, source} = req.body
   const condition = (en || sr) && author
-  if (!condition) return res.send('ARGUMENTS_ERROR')
+  if (!condition) return res.send({message: 'ARGUMENTS_ERROR'})
 
   SacredThought.find({$or: [{sr}, {en}]}, (err, results) => {
-    if (results.length) return res.send('ALREADY_EXISTS')
+    if (results.length) return res.send({message: 'ALREADY_EXISTS'})
   })
 
   SacredThought.create({en, sr, author, source}, (err, quote) => {
     if (err) return console.error(err)
-    res.send('SUCCESS_SAVED')
+    res.send({message: 'SUCCESS_SAVED', quote})
   })
 }
