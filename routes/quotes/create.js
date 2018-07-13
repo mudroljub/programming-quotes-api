14c4c3f1280d8872882aab529bc16d9fee2249d6
Damjan Pavlica <mudroljub@gmail.com>
2018-07-13 12:57:45 +0200

    ukinuta provera da li postoji

--- routes/quotes/create.js
+++ routes/quotes/create.js
@@ -5,9 +5,9 @@ module.exports =  (req, res) => {
   const condition = (en || sr) && author
   if (!condition) return res.send({message: 'ARGUMENTS_ERROR'})
 
-  ProgrammingQuote.find({$or: [{sr}, {en}]}, (err, results) => {
-    if (results.length) return res.send({message: 'ALREADY_EXISTS'})
-  })
+  // ProgrammingQuote.find({$or: [{sr}, {en}]}, (err, results) => {
+  //   if (results.length) return res.send({message: 'ALREADY_EXISTS'})
+  // })
 
   ProgrammingQuote.create({en, sr, author, source}, (err, quote) => {
     if (err) return console.error(err)
--- routes/thoughts/create.js
+++ routes/thoughts/create.js
@@ -5,9 +5,9 @@ module.exports = (req, res) => {
   const condition = (en || sr) && author
   if (!condition) return res.send({message: 'ARGUMENTS_ERROR'})
 
-  SacredThought.find({$or: [{sr}, {en}]}, (err, results) => {
-    if (results.length) return res.send({message: 'ALREADY_EXISTS'})
-  })
+  // SacredThought.find({$or: [{sr}, {en}]}, (err, results) => {
+  //   if (results.length) return res.send({message: 'ALREADY_EXISTS'})
+  // })
 
   SacredThought.create({en, sr, author, source}, (err, quote) => {
     if (err) return console.error(err)
