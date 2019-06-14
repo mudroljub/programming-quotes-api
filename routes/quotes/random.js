0aca0d5e52d5a9895ea0c9808bc92acfd0477baa
Damjan Pavlica <mudroljub@gmail.com>
2019-06-15 01:20:57 +0200

    lint fix

--- routes/quotes/random.js
+++ routes/quotes/random.js
@@ -3,6 +3,6 @@ module.exports = (req, res) => {
 
   Quote.count().exec((err, count) => {
     const rand = Math.floor(Math.random() * count)
-    Quote.findOne().select({ "author": 1, "en": 1, "_id": 0}).skip(rand).exec((err, quote) => res.send(quote))
+    Quote.findOne().select({ 'author': 1, 'en': 1, '_id': 0}).skip(rand).exec((err, quote) => res.send(quote))
   })
 }
