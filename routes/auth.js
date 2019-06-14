f9f8e05f439fba78def76f4fc1dc2565815dc75c
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-14 15:05:22 +0200

    handle multiple clients auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,21 @@ const router = require('express').Router()
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+let clientDomain = ''
 
-router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-  const token = jwt.sign({user: req.user}, process.env.JWTSECRET)
-  res.redirect(`/auth/google/${token}`)
+// prihvata zahtev klijenta i salje na google login
+router.get('/google', (req, res, next) => {
+  clientDomain = req.headers.referer
+  passport.authenticate('google', { scope: ['profile'] })(req, res, next)
 })
 
+// prihvata token od googla nakon autentifikacije i salje klijentu
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+  const token = jwt.sign({user: req.user}, process.env.JWTSECRET)
+  res.redirect(`${clientDomain}#/auth/google/${token}`)
+})
+
+// proverava token i nalazi korisnika
 router.get('/google/:token', (req, res) => {
   jwt.verify(req.params.token, process.env.JWTSECRET, (err, user) => {
     if (err) res.send({res: false})
