abebf067b6771a479a63484e3b5a705d85eec278
Damjan Pavlica <mudroljub@gmail.com>
2019-06-17 00:45:41 +0200

    fix lint

--- routes/quotes/vote.js
+++ routes/quotes/vote.js
@@ -1,17 +1,14 @@
-module.exports = (req, res) => {
+module.exports = async(req, res) => {
   const {_id, newVote} = req.body
   const { Quote } = res.locals
+  if (newVote > 5 || newVote < 1) return res.status(400).send({message: 'Invalid vote'})
 
-  Quote.findById(_id, (err, quote) => {
+  const quote = await Quote.findById(_id)
+  const { rating, numberOfVotes } = quote
+  const newRating = (rating * numberOfVotes + Number(newVote)) / (numberOfVotes + 1)
+  quote.rating = newRating.toFixed(2)
+  quote.save(err => {
     if (err) return console.error(err)
-    if (newVote > 5 || newVote < 1) return res.status(400).send({message: 'Invalid vote'})
-
-    const { rating, numberOfVotes } = quote
-    const newRating = (rating * numberOfVotes + Number(newVote)) / (numberOfVotes + 1)
-    quote.rating = newRating.toFixed(2)
-    quote.save(err => {
-      if (err) return console.error(err)
-      res.send({message: 'SUCCESS_SAVED', quote})
-    })
+    res.send({message: 'SUCCESS_SAVED', quote})
   })
 }
