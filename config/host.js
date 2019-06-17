d0afacc7dea9013cd584d9a36dcd17888f7211fe
Restyled.io <commits@restyled.io>
2019-06-17 14:16:44 +0000

    Restyled by prettier

--- config/host.js
+++ config/host.js
@@ -1,10 +1,11 @@
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-const domain = process.env.NODE_ENV === 'development'
-  ? `http://localhost:${port}`
-  : 'https://programming-quotes-api.herokuapp.com'
+const domain =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:${port}`
+    : "https://programming-quotes-api.herokuapp.com";
 
 module.exports = {
   port,
-  domain,
-}
+  domain
+};
