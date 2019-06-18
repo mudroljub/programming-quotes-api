a465ded6400bd386c799af0fa778bd9b4c9069ac
Damjan Pavlica <damjan.pavlica@origo.is>
2019-06-18 16:12:44 +0200

    fix votes

--- routes/quotes/vote.js
+++ routes/quotes/vote.js
@@ -5,14 +5,17 @@ module.exports = async(req, res) => {
   const { Quote, user } = res.locals
   if (newVote > 5 || newVote < 1) return res.status(400).send({message: 'Invalid vote'})
 
-  const quote = await Quote.findById(quoteId)
-  const { rating, numberOfVotes } = quote
-  const newRating = (rating * numberOfVotes + Number(newVote)) / (numberOfVotes + 1)
-  quote.rating = newRating.toFixed(1)
-
-  quote.save(err => {
-    if (err) return res.status(500).send(err.message)
-    if (user) User.update({_id: user._id}, {$addToSet: {voted: quoteId}})
-    res.send({message: 'SUCCESS_SAVED', quote})
-  })
+  try {
+    const quote = await Quote.findById(quoteId)
+    const { rating, numberOfVotes } = quote
+    const newRating = (rating * numberOfVotes + Number(newVote)) / (numberOfVotes + 1)
+    quote.rating = newRating.toFixed(1)
+    quote.save(err => {
+      if (err) return res.status(500).send(err.message)
+      if (user) User.update({_id: user._id}, {$addToSet: {voted: quoteId}})
+      res.send({message: 'SUCCESS_SAVED', quote})
+    })
+  } catch (e) {
+    res.send({message: e.message })
+  }
 }
