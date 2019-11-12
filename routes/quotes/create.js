03b9bd38ebeedeeb3003a2750690441caeed10e2
Damjan Pavlica <damjan.pavlica@origo.is>
2019-11-12 13:18:14 +0100

    try fix

--- routes/quotes/create.js
+++ routes/quotes/create.js
@@ -1,7 +1,9 @@
 module.exports =  (req, res) => {
   const { Quote, user } = res.locals
+  const params = {...req.body}
+  delete params._id
 
-  Quote.create({...req.body, addedBy: user._id }, (err, quote) => {
+  Quote.create({ ...params, addedBy: user._id }, (err, quote) => {
     if (err) return res.status(500).send(err)
     res.send({message: 'SUCCESS_SAVED', quote})
   })
