1f7934fd3132dfeda83ec21087ccb1e6d3945455
Damjan Pavlica <damjan.pavlica@origo.is>
2019-11-12 11:33:40 +0100

    update mongo, remove tags

--- routes/quotes/read.js
+++ routes/quotes/read.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   const { Quote } = res.locals
   Quote
     .find()
-    .select({ author: 1, en: 1, sr: 1, tags:1, rating: 1 })
+    .select()
     .then(quotes => res.send(quotes))
     .catch(e => res.send('SERVER_ERROR', e.message))
 }
