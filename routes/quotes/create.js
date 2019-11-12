02df8d60019c5dd6ca071df1a8299b6808e695ba
Damjan Pavlica <damjan.pavlica@origo.is>
2019-11-12 11:49:26 +0100

    update and post quote

--- routes/quotes/create.js
+++ routes/quotes/create.js
@@ -1,8 +1,7 @@
 module.exports =  (req, res) => {
-  const {en, sr, author, source} = req.body
   const { Quote, user } = res.locals
 
-  Quote.create({en, sr, author, source, addedBy: user._id }, (err, quote) => {
+  Quote.create({...req.body, addedBy: user._id }, (err, quote) => {
     if (err) return res.status(500).send(err)
     res.send({message: 'SUCCESS_SAVED', quote})
   })
--- routes/quotes/update.js
+++ routes/quotes/update.js
@@ -1,10 +1,10 @@
 module.exports = (req, res) => {
-  const {_id, en, sr, author, tags, source} = req.body
+  const {_id} = req.body
   const { Quote } = res.locals
 
-  Quote.findById(_id, (err, quote) => {  
+  Quote.findById(_id, (err, quote) => {
     if (err) return console.error(err)
-    quote.set({ en, sr, author, source, tags })
+    quote.set({...req.body})
     quote.save(err => {
       if (err) return console.error(err)
       res.send({message: 'SUCCESS_SAVED', quote})
