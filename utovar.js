49b770d4adf780b595163421691a68b0c1ebb27e
Damjan Pavlica <damjan.pavlica@origo.is>
2019-12-24 16:29:40 +0100

    m

--- utovar.js
+++ utovar.js
@@ -11,22 +11,18 @@ const citati = require('./citati.json')
 mongoose.connect(mongoUri, { useNewUrlParser: true })
 mongoose.set('useCreateIndex', true)
 
-// const filtrirano = citati
-//   .filter(q => q.author == 'Isus' && (q.source.includes('Jovanu') || q.source.includes('Mateju') || q.source.includes('Luki') || q.source.includes('Marku')))
-//   .map(({_id, sr, ms, source}) => ({_id, ms, source}))
-// fs.writeFileSync('filtrirano.json', JSON.stringify(filtrirano, null, 2))
-
-// Quote.find()
-//   .then(res => {
-//     const filtrirano = res
-//       // .filter(q => q.author == 'Isus' && (q.source.includes('Tomi')))
-//       .map(({_id, ms, source}) => ({_id, ms, source }))
-//     fs.writeFileSync('citati.json', JSON.stringify(filtrirano, null, 2))
-//   })
-
-citati.forEach(q => {
-  Quote.findOne({_id: q._id}, (err, obj) => {
-    obj.ms = q.ms
-    obj.save()
+Quote.find()
+  .then(res => {
+    const filtrirano = res
+      .filter(q => !q.ms)
+      .map(({_id, ms, sr, source }) => ({_id, ms, sr, source }))
+    fs.writeFileSync('citati.json', JSON.stringify(filtrirano, null, 2))
   })
-})
+
+// citati.forEach(q => {
+//   Quote.findOne({_id: q._id}, (err, obj) => {
+//     obj.sr = q.sr
+//     obj.ms = q.ms
+//     obj.save()
+//   })
+// })
