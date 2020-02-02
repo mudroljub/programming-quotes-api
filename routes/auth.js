1fae1835e739ab3d448f210a8d0d70a28669c1b9
Damjan Pavlica <mudroljub@gmail.com>
2020-02-02 22:28:23 +0100

    update auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,25 @@ const router = require('express').Router()
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
 
-let clientDomain = ''
+let referrer = ''
 
 const providers = ['google', 'github']
 for (const provider of providers) {
   // login
   router.get(`/${provider}`, (req, res, next) => {
-    clientDomain = req.headers.referer
+    referrer = req.headers.referer
     passport.authenticate(provider)(req, res, next)
   })
 
   // pass token from provider to client on callback
   router.get(`/${provider}/redirect`, passport.authenticate(provider), (req, res) => {
     const token = jwt.sign({user: req.user}, process.env.JWTSECRET)
-    res.redirect(`${clientDomain}#/auth/${provider}/${token}`)
+    if (referrer.includes('programming-quotes'))
+      res.redirect(`${referrer}#/auth/${provider}/${token}`)
+    else {
+      const clientDomain = referrer.split('/')[2]
+      res.redirect(`${clientDomain}/auth/${provider}/${token}`)
+    }
   })
 
   // find user
