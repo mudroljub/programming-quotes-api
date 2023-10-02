3a54a623ab3b40b7e79e30d1451252f59b72769e
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-10-01 22:28:47 -0600

    fix: add and preserve favorite quotes

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -5,7 +5,6 @@ using ProgrammingQuotesApi.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using ProgrammingQuotesApi.Services.Interfaces;
 
 namespace ProgrammingQuotesApi.Services
 {
