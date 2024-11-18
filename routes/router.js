5f60f7cd80404cf968165594cc5b078c9b84bdac
Damjan Pavlica <mudroljub@gmail.com>
2024-11-18 22:56:15 +0100

    m

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,8 @@
 const router = require('express').Router()
-const Quote = require('../models/Quote')
 const quotesRouter = require('./quotes/')
 const authRouter = require('./auth')
 
-router.use('/quotes', (req, res, next) => {
-  res.locals.Quote = Quote
-  next()
-}, quotesRouter)
-
+router.use('/quotes', quotesRouter)
 router.use('/auth', authRouter)
 
 module.exports = router
