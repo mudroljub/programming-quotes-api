0237d948d345b97567e0770f4ca9ea2fc805009f
Damjan Pavlica <mudroljub@gmail.com>
2024-11-19 17:57:52 +0100

    m

--- models/Quote.js
+++ models/Quote.js
@@ -8,19 +8,26 @@ const QuotesSchema = new Schema({
     type: String,
     required: true,
     trim: true,
+    minlength: 2,
+    maxlength: 100,
   },
   en: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
+    minlength: 5,
+    maxlength: 800,
   },
   sr: {
     type: String,
     trim: true,
+    minlength: 5,
+    maxlength: 800,
   },
   source: {
     type: String,
     trim: true,
+    maxlength: 255,
   },
   addedBy: {
     type: Schema.Types.ObjectId,
