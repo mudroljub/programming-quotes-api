b993897e85baca3234713499481cae1aeb2a532b
Damjan Pavlica <mudroljub@gmail.com>
2024-11-23 13:19:44 +0100

    m

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 import 'dotenv/config.js'
-import fs from 'fs'
+import { promises as fs } from 'fs'
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import { marked } from 'marked'
-import { promisify } from 'util'
 
 import { port, domain } from './config/host.js'
 import router from './routes/router.js'
 import { normalizeJsonKeys } from './middleware/normalize.js'
 
 const app = express()
-const readFileAsync = promisify(fs.readFile)
 
 // MIDDLEWARES
 app.use(cors())
@@ -21,15 +19,15 @@ app.use(bodyParser.json())
 app.use(normalizeJsonKeys)
 
 mongoose.connect(process.env.CONNECTION_STRING)
-  .catch(err => console.error('MongoDB connection error:', err))
+  .catch(err => console.error('Could not connect to database:', err))
 
 // ROUTES
 app.get('/', async(req, res) => {
   try {
-    const file = await readFileAsync('README.md', 'utf8')
-    res.send(marked(file.toString()))
+    const data = await fs.readFile('README.md', 'utf8')
+    res.send(marked(data))
   } catch (err) {
-    res.status(500).send('Error reading file')
+    res.status(500).send(`Error reading file: ${err.message}`)
   }
 })
 
