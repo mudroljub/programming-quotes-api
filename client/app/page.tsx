2b2886019c76671037ac9b8b39f0e0e867de382a
Damjan Pavlica <mudroljub@gmail.com>
2024-11-25 17:06:25 +0100

    naslovna

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,18 @@
-export default function Home() {
+import Link from 'next/link'
+import { Quote } from '../types/quote';
+
+export default async function Home(): Promise<JSX.Element> {
+  const res = await fetch('http://localhost:5000/api/quotes/5a9b21892bad9600044b7006');
+  const quote: Quote = await res.json();
+
+  const authorLink: string = `https://en.wikipedia.org/wiki/${quote.author.replace(/ /g, '_')}`;
+
   return (
-    <h1>Zdravo svete</h1>
+    <blockquote>
+      <p className="quote-text">
+        {quote.text}
+      </p>
+      <span> — <Link href={authorLink} target='_blank' className="hover:underline">{quote.author}</Link></span>
+    </blockquote>
   );
 }
---
+++ client/types/quote.d.ts
@@ -0,0 +1,6 @@
+export type Quote = {
+  _id: string;
+  author: string;
+  text: string;
+  source: string;
+};
