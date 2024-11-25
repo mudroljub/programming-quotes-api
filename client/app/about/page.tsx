053c57a03b4f45c4d053661696ea52b5982eddc1
Damjan Pavlica <mudroljub@gmail.com>
2024-11-26 00:09:09 +0100

    m

--- client/app/about/page.tsx
+++ client/app/about/page.tsx
@@ -1,10 +1,12 @@
 export default function About(): JSX.Element {
   return (
     <div>
-      <h1>About Us</h1>
       <p>
-        Welcome to the About page! This is where you can learn more about our project.
+      Programming Quotes is a place where I’ve been collecting quotes and thoughts from the world of programming for years. These quotes are my way of reminding myself why I love coding.</p> 
+      
+      <p className="mt-4">From deep wisdom to fun ones that remind us not to take ourselves too seriously – there’s something here for every programmer. Whether you’re a veteran or just starting out, I hope you enjoy the quotes and maybe find one that speaks to you.
       </p>
+      <p className="mt-4"><a href="https://github.com/mudroljub">mudroljub</a></p>
     </div>
   );
 }
---
+++ client/app/stats/page.tsx
@@ -0,0 +1,10 @@
+export default function About(): JSX.Element {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+      Programming Quotes is a place where I collect quotes and thoughts from the world of programming that have inspired me over the years. From wisdom that makes us think, to the fun ones that remind us not to take ourselves too seriously – there's something here for every programmer. This site is my way of sharing those little moments of inspiration and reminding myself (and you) why we love coding. Whether you're a programming veteran or just starting out, I hope you enjoy the quotes and maybe find one that speaks to you.
+      </p>
+    </div>
+  );
+}
