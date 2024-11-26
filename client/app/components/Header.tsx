62f5aea39e200da5397ea66848c44d1ed7d9387e
Damjan Pavlica <mudroljub@gmail.com>
2024-11-26 10:35:27 +0100

    active nav

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,16 +1,31 @@
+'use client'
+import { usePathname } from 'next/navigation'
+
 export default function Header() {
+  const pathname = usePathname()
+
+  function isActive(link: string): string {
+    return `hover:underline ${pathname === link ? 'font-bold' : ''}`
+  }
+
   return (
     <header className="bg-blue-500 text-white p-4">
-    <div className="max-w-4xl mx-auto">
-      <h1 className="text-xl">Programming Quotes</h1>
-      <nav>
-        <ul className="flex space-x-4">
-          <li><a href="/" className="hover:underline">Home</a></li>
-          <li><a href="/stats" className="hover:underline">Stats</a></li>
-          <li><a href="/about" className="hover:underline">About</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-  );
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-xl">Programming Quotes</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/" className={isActive('/')}>Home</a>
+            </li>
+            <li>
+              <a href="/stats" className={isActive('/stats')}>Stats</a>
+            </li>
+            <li>
+              <a href="/about" className={isActive('/about')}>About</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
