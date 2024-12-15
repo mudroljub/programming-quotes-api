5f29ba1f2b1e8169286c5aabd893f1f9952815bc
Damjan Pavlica <mudroljub@gmail.com>
2024-12-15 22:30:02 +0100

    return user

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,8 +6,7 @@ const getToken = async(req, res) => {
   try {
     const user = await UserService.findOrCreate(email, password)
     const token = AuthService.createToken(user)
-    res.json({ message: 'Welcome to Programming Quotes API', token })
-
+    res.json({ message: 'Welcome to Programming Quotes API', token, user })
   } catch (err) {
     const status = err.message === 'BAD_PASSWORD' ? 400 : 500
     res.status(status).json({ message: err.message })
